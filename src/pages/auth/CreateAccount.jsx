f6062aa8e4abe2df426d0dd12e8eea59bdf11546
Arun Negi <Arun-Negi989>
2025-12-30 15:29:54 +0530

    resolve api issue

--- src/pages/auth/CreateAccount.jsx
+++ src/pages/auth/CreateAccount.jsx
@@ -15,6 +15,9 @@ const CreateAccount = () => {
 
   const [loading, setLoading] = useState(false);
 
+  // ✅ CHANGE HERE (ENV BASE URL)
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -28,7 +31,7 @@ const CreateAccount = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        `${API_BASE_URL}/api/auth/register`,
         formData,
         {
           headers: {
